c1cf9ef4206d90859c4ae16a733a94b92778c604
daniellerodrigues <daniellerodrigues-14@live.com>
2022-11-17 15:43:54 -0300

    add dom manipulation

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleMove(){
+    document.querySelector('#title').style.color = '#FF79C6';
+}
+
+   
+
+function handleOut(){
+    document.querySelector('#title').style.color = 'white';
+  }
